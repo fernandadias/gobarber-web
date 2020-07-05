8395cc9077315a119ad5b73d93a9f54c572ba152
Nanda Dias <fernanda.dias@konduto.com>
2020-07-05 14:42:15 -0300

    Cria a estrutura da página de cadastro

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import SignIn from './pages/SingIn'
+import SignIn from './pages/SingIn';
+import SingUp from './pages/SingUp';
 
 import GlobalStyles from './styles/global'
 
 const App: React.FC = () => (
   <>
-    <SignIn />
+    <SingUp />
     <GlobalStyles />
   </>
 )
---
+++ src/pages/SingUp/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { FiArrowLeft, FiLock, FiUser, FiMail } from 'react-icons/fi';
+
+import LogoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={LogoImg} alt="GoBarber" />
+
+      <form action="">
+        <h1>Faça seu cadastro</h1>
+        <Input icon={FiUser} name="name" placeholder="Nome" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+        <Button type="submit">Cadastrar</Button>
+
+      </form>
+
+      <a href=""><FiArrowLeft />Voltar para logon</a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
