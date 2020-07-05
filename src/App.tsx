5595296ab9a07b136711fe1f0d713e85efa6d209
Nanda Dias <fernanda.dias@konduto.com>
2020-07-04 23:09:23 -0300

    Aplica os estilos globais

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import GlobalStyles from './styles/global'
+
 const App: React.FC = () => (
-  <h1>Ma oee</h1>
+  <>
+    <h1>Ma oee</h1>
+    <GlobalStyles />
+  </>
 )
 
 export default App;
