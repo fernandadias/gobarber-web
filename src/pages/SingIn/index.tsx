be3597b617e728937987b9fc2c7067e642d515e2
Nanda Dias <fernanda.dias@konduto.com>
2020-07-05 13:58:51 -0300

    Isola os componentes de Botão e Input

--- src/pages/SingIn/index.tsx
+++ src/pages/SingIn/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
-import LogoImg from '../../assets/logo.svg'
+import LogoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import { Container, Content, Background } from './styles';
 
 const SignIn: React.FC = () => (
@@ -11,9 +15,9 @@ const SignIn: React.FC = () => (
 
       <form action="">
         <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
-        <button type="submit">Entrar</button>
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input icon={FiLock} name="password" type="password" placeholder="Senha" />
+        <Button type="submit">Entrar</Button>
 
         <a href="forgot">Esqueci minha senha</a>
       </form>
